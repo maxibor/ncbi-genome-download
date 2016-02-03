8f85ad9de427431ae13562576ea4cd69302440be
Maxime <Maxime@MacBook-Pro-de-Maxime.local>
2016-02-03 19:21:49 +0100

    Adding the program and genome.txt file

---
+++ bacterial_genome_download.py
@@ -0,0 +1,38 @@
+import csv
+import sys
+import os
+# name_list = open("genomes.txt" , "r")
+try :
+    with open(sys.argv[1],"r") as name_list :
+        with open("assembly_summary_refseq.txt", "r") as genome_list_file :
+            genomes_list = csv.reader(genome_list_file, delimiter = "\t")
+
+            for name in name_list :
+                name = name.rstrip()
+                for genome in genomes_list :
+                    if name == genome[7] :
+                        strain_name = genome[0]+"_"+genome[15]
+                        file_name = strain_name+"_genomic.fna.gz"
+                        print genome
+                        link = genome[-1]
+                        break
+                cmd = "wget "+link+"/*.fna.gz &> /dev/null"
+                print "Downloading the genome of "+name+" strain "+strain_name+" ..."
+                os.system(cmd)
+                print "Finished Downloading "+file_name
+except IndexError :
+    print '''
+    ==NCBI BACTERIAL GENOME DOWNLOAD==
+
+    -> Usage : python bacterial_genome_download.py genomes.txt
+
+    -> genomes.txt must contain one bacterial specie name per line
+    '''
+except IOError :
+    print '''
+    ==NCBI BACTERIAL GENOME DOWNLOAD==
+
+    -> Usage : python bacterial_genome_download.py genomes.txt
+
+    -> genomes.txt must contain one bacterial specie name per line
+    '''
